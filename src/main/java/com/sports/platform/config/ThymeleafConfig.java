86db2fc50e3b25932dbefdd5bbd9031f07f3bb42
Fix <fix@sports.com>
2026-04-14 18:51:28 +0800

    fix: 移除thymeleaf-extras-java8time依赖，添加Java8Time格式化配置

--- src/main/java/com/sports/platform/config/ThymeleafConfig.java
+++ src/main/java/com/sports/platform/config/ThymeleafConfig.java
@@ -3,8 +3,11 @@ package com.sports.platform.config;
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.time.format.DateTimeFormatter;
 
 /**
  * Web MVC 配置
@@ -18,6 +21,17 @@ public class ThymeleafConfig implements WebMvcConfigurer {
     @Bean
     public LayoutDialect layoutDialect() {
         return new LayoutDialect();
+    }
+
+    /**
+     * 添加 Java 8 Time 格式化支持
+     */
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addFormatterForFieldType(java.time.LocalDate.class,
+                new org.springframework.format.datetime.joda.LocalDateFormatter(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        registry.addFormatterForFieldType(java.time.LocalDateTime.class,
+                new org.springframework.format.datetime.joda.LocalDateTimeFormatter(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
     }
 
     /**
